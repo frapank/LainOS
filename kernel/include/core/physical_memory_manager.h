a7d50e92b443f0d78c92c284af9698ba8d2de562
frapank <francesco.pankov@outlook.it>
2026-03-05 14:01:25 +0000

    Add physical memory manager

---
+++ kernel/include/core/physical_memory_manager.h
@@ -0,0 +1,13 @@
+#ifndef PHYSICAL_MEMORY_MANAGMENT_H
+#define PHYSICAL_MEMORY_MANAGMENT_H
+
+#include "utils/types.h"
+
+_Bool phmm_is_address_free(u32 address);
+void phmm_mark_region_used(u32 init_address, u32 size);
+void phmm_mark_region_free(u32 init_address, u32 size);
+u32* phmm_alloc_blocks(u32 num);
+void phmm_free_blocks(u32* addr, u32 num_block);
+u32 phmm_init(u32 init_address, u32 size);
+
+#endif
--- kernel/src/kernel.c
+++ kernel/src/kernel.c
@@ -4,11 +4,13 @@
 #include "core/boot_info.h"
 #include "core/print_vga_text.h"
 #include "core/paging.h"
+#include "core/physical_memory_manager.h"
 #include "core/panic.h"
 #include "core/kernel_context.h"
 #include "utils/kshell/kshell.h"
 
 #define BOOT_IDENTIFIER 0x1BADB002
+#define KERNEL_END_ADDRESS 0x00100000 // May change
 
 static u32 calculate_total_memory(struct boot_info* boot_info)
 {
@@ -24,9 +26,20 @@ static void system_init(struct kernel_context* ctx, u32 magic)
     if(magic != BOOT_IDENTIFIER)
         panick("Wrong bootloader identifier");
 
-    paging_init(ctx);
     idt_init();
     pic_init();
+
+    u32 alloc_bytes = (ctx->system_ram_kb * 1024);
+    u32 bitmap_size = phmm_init(KERNEL_END_ADDRESS, alloc_bytes);
+    if (bitmap_size == 0 || bitmap_size > alloc_bytes) 
+        panick("phmm_init failed");
+
+    u32 start_region = 0x00000000;
+    u32 bytes_to_reserve = (KERNEL_END_ADDRESS - start_region) + bitmap_size;
+    phmm_mark_region_used(start_region, bytes_to_reserve);
+
+    //paging_init(ctx);
+
     sti();   
 }
 
