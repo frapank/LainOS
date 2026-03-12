659785d881162e068307c5422a64ccc0c9c78323
frapank <francesco.pankov@outlook.it>
2026-03-12 20:13:30 +0000

    Add new ALWAYS_INLINE macro

--- kernel/include/utils/defhelp.h
+++ kernel/include/utils/defhelp.h
@@ -1,7 +1,8 @@
 #ifndef DEFHELP_H
 #define DEFHELP_H
 
-#define likely(x)   __builtin_expect(!!(x), 1)
-#define unlikely(x) __builtin_expect(!!(x), 0)
+#define likely(x)           __builtin_expect(!!(x), 1)
+#define unlikely(x)         __builtin_expect(!!(x), 0)
+#define ALWAYS_INLINE       __always_inline 
 
 #endif
