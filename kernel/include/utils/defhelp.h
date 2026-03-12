706baafc99b4e28a01127fa09cf62be9877fca16
frapank <francesco.pankov@outlook.it>
2026-03-12 20:41:36 +0000

    Add compatibilty check

--- kernel/include/utils/defhelp.h
+++ kernel/include/utils/defhelp.h
@@ -1,8 +1,15 @@
 #ifndef DEFHELP_H
 #define DEFHELP_H
 
+#if defined(__clang__) || defined(__GNUC__)
 #define likely(x)           __builtin_expect(!!(x), 1)
 #define unlikely(x)         __builtin_expect(!!(x), 0)
 #define ALWAYS_INLINE       __always_inline 
+#else
+#define likely(x)
+#define unlikely(x)
+#define ALWAYS_INLINE
+#endif
+
 
 #endif
