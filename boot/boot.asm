26aae334e044ca55af1d7a1eb81a82729311b821
frapank <francesco.pankov@outlook.it>
2026-03-17 07:11:37 +0000

    Comments adjustament

--- boot/boot.asm
+++ boot/boot.asm
@@ -19,13 +19,12 @@ start:
     mov ds, ax              ; Set Dat Segment (to zero)
     mov es, ax              ; Set Extra Segment (to zero)
 
-    ; Set boot disk
-    mov [boot_disk], dl     ; Store boot drive into boot_disk (used in read_disk)
+    ; Store boot disk used in stage 2
+    mov [boot_disk], dl
     mov byte [0x7E00], dl
 
     call clear_screen
 
-    ; Load segment and load second mode
     call read_disk
     call start_second
 
@@ -33,28 +32,28 @@ start:
 ; Data
 msg_disk_error          db "[-] Can't find second stage, stopped" ,0Dh,0Ah,0
 
-boot_disk               db  0         ; Store boot drive number
-STAGE_LOCATION          equ 0x1000    ; Load address for second stage
-STAGE_SECTORS           equ 2         ; Number of sectors to read
-STAGE_START_LBA         equ 2048      ; First sector of second stage
+boot_disk               db  0           ; Store boot drive number
+STAGE_LOCATION          equ 0x1000      ; Load address for second stage
+STAGE_SECTORS           equ 2           ; Number of sectors to read
+STAGE_START_LBA         equ 2048        ; First sector of second stage
 dap:
-    db 0x10              ; packet size = 16 bytes
-    db 0                 ; reserved
-    dw STAGE_SECTORS     ; number of sectors
-    dw STAGE_LOCATION    ; offset
-    dw 0x0000            ; segment
-    dq STAGE_START_LBA   ; LBA start
+    db 0x10                             ; packet size = 16 bytes
+    db 0                                ; reserved
+    dw STAGE_SECTORS                    ; number of sectors
+    dw STAGE_LOCATION                   ; offset
+    dw 0x0000                           ; segment
+    dq STAGE_START_LBA                  ; LBA start
 
 
 ;------------------
 ; Includes
-%include "boot/graphics.asm" ; Include (copy) print_string, clear_screen and wait_key
+%include "boot/graphics.asm"
 
 ;------------------
 ; Setup Protected
 read_disk:
-    lea si, [dap] ; DS:SI -> packet 
-    mov ah, 0x42 ; extended read 
+    lea si, [dap]               ; DS:SI -> packet 
+    mov ah, 0x42                ; extended read 
     mov dl, [boot_disk] 
     int 0x13 
     jc disk_error 
